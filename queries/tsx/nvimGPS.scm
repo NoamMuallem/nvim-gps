dac064a55dee5ae6cc8999b8a1fded1242b77616
Noam <40291455+NoamMuallem@users.noreply.github.com>
2022-05-03 21:20:02 +0300

    Update nvimGPS.scm

--- queries/tsx/nvimGPS.scm
+++ queries/tsx/nvimGPS.scm
@@ -11,3 +11,22 @@
   value: (call_expression
     function: (identifier) @hook-identifier (#match? @hook-identifier "^use")
   )) @scope-root)
+
+; JSX
+((jsx_element
+	open_tag: (jsx_opening_element
+    name: (identifier) @tag-name
+  )) @scope-root)
+
+((jsx_element
+	open_tag: (jsx_opening_element
+    name: (nested_identifier) @tag-name
+  )) @scope-root)
+
+((jsx_self_closing_element
+    name: (identifier) @tag-name
+  ) @scope-root)
+
+((jsx_self_closing_element
+    name: (nested_identifier) @tag-name
+  ) @scope-root)
