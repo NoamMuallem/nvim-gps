1c25e3a98fb886a660e51e2f8f50c7e361cf6721
Noam <40291455+NoamMuallem@users.noreply.github.com>
2022-05-03 21:20:30 +0300

    Update nvimGPS.scm

--- queries/jsx/nvimGPS.scm
+++ queries/jsx/nvimGPS.scm
@@ -1,1 +1,33 @@
 ; inherits: javascript
+
+
+; React hook name
+((call_expression
+	function: (identifier) @hook-name (#match? @hook-name "^use")
+	) @scope-root)
+
+; React hook function name
+((variable_declarator
+  name: (identifier) @function-name
+  value: (call_expression
+    function: (identifier) @hook-identifier (#match? @hook-identifier "^use")
+  )) @scope-root)
+
+; JSX
+((jsx_element
+	open_tag: (jsx_opening_element
+    name: (identifier) @tag-name
+  )) @scope-root)
+
+((jsx_element
+	open_tag: (jsx_opening_element
+    name: (nested_identifier) @tag-name
+  )) @scope-root)
+
+((jsx_self_closing_element
+    name: (identifier) @tag-name
+  ) @scope-root)
+
+((jsx_self_closing_element
+    name: (nested_identifier) @tag-name
+  ) @scope-root)
